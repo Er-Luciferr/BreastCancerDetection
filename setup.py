b5c810583f8e03e247d9b0383103d02d7ba7f410
Er-Luciferr <mohitsingh2222.ms@gmail.com>
2024-01-19 21:28:55 +0530

    architecture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from typing import List
+from setuptools import find_packages,setup
+
+setup(
+    name="Breast Cancer Detection",
+    version='0.0.1',
+    author='Mohit Singh',
+    author_email='mohitsingh2@yahoo.com',
+    packages=find_packages(),
+    install_requires=['requirements.txt']
+)
